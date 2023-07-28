259d1b3c20fd786602bdc2e446fcfb559f10232f
juan <juancastpro34@gmail.com>
2023-07-27 21:11:04 -0500

    fusion del menu mobile, media query para ver menus en donde son, toggle e inactive para ello en js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navEmail = document.querySelector('.nav-email')
 const menuEmail = document.querySelector('.menu-email')
 
+const mobileMenu = document.querySelector('.mobile-menu')
+const burguerMenu = document.querySelector('.burguerMenu')
+
 navEmail.addEventListener('click', toggleMenuEmail)
+burguerMenu.addEventListener('click', toggleMobileMenu)
 
 function toggleMenuEmail(){
     menuEmail.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
